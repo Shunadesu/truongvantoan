540d765427a21a8cca6749ca231ba26f96f98c85
shunadesu <namp280918@gmail.com>
2025-09-28 18:22:56 +0700

    update

--- src/data/dich_vu.js
+++ src/data/dich_vu.js
@@ -1,15 +1,6 @@
 import { accImages } from '../assets/images';
 
   export const dichVu = [
-  {
-    id: 501,
-    name: 'Acc Clone chưa tạo HLV',
-    price: 'Từ 20.000đ',  
-    description: 'Trắng thông tin',
-    status: 'Còn',
-    image: accImages.img_accclone,
-    metaTitle: 'Acc Clone chưa tạo HLV - truongvantoan.com',
-    metaDescription: 'Dịch vụ acc clone chưa tạo HLV, trắng thông tin, còn 498 tài khoản tại truongvantoan.com.',
-  },
+
  
 ]; 
